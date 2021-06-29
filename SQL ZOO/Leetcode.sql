cadb46aaf55b71569b83b23645984ed82966f155
Ayonika Bose <83725663+nikabosedata@users.noreply.github.com>
2021-06-29 10:57:20 -0500

    Update Leetcode.sql

--- SQL ZOO/Leetcode.sql
+++ SQL ZOO/Leetcode.sql
@@ -13,6 +13,10 @@ select Email from Person
 group by Email
 having count(Email) > 1;
 
+--3. Write an SQL query to find all dates' id with higher temperature compared to its previous dates (yesterday). Return the result table in any order.
+ SELECT weather.id FROM Weather 
+ JOIN weather w ON DATEDIFF(weather.recordDate, w.recordDate) = 1
+        AND weather.Temperature > w.Temperature
 
 
 
